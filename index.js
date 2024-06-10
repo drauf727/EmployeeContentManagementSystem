ad5b845e495fad88f1f137a6feaa16ebd92bd375
David Rauf <davidrauf@gmail.com>
2024-06-10 19:08:19 -0400

    almost done

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 const pg = require('pg');
-const express = require('express');
 
 const { Pool } = require('pg');
 
@@ -20,22 +19,37 @@ const pool = new Pool(
   pool.connect();
 
 const options = [
-    'View all departments', 'View all rows', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'Exit'
+    'View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'Exit'
   ]
 
-const viewAllDepartments = async() => {
-  const connect = await pool.connect();
-  try {
-    const res = await connect.query('SELECT name FROM department');
-    console.table(res.rows);
-  } catch (err) {
-    console.log(err)
-  } finally {
-    connect.release();
-  }
-  };
+  const connectDBView = async(command) => {
+    const connect = await pool.connect();
+    try {
+      const res = await connect.query(command);
+      console.table(res.rows);
+      prompt();
+    } catch (err) {
+      console.log(err)
+    } finally {
+      connect.release();
+    }
+    };
 
-inquirer.prompt(
+    const connectDBEdit = async(command) => {
+      const connect = await pool.connect();
+      try {
+        const res = await connect.query(command);
+        console.log('Your request was successful');
+        prompt();
+      } catch (err) {
+        console.log(err)
+      } finally {
+        connect.release();
+      }
+      };
+
+const prompt = () => {
+  inquirer.prompt(
   [{
     name: 'Options',
     message: 'Choose an option below:',
@@ -47,11 +61,68 @@ inquirer.prompt(
 .then((answer) => {
   switch (answer.Options){
     case "View all departments":
-      viewAllDepartments();
+      connectDBView('SELECT * FROM department');
       break;
+      case "View all roles":
+        connectDBView('SELECT * FROM role');
+        break;
+        case "View all employees":
+          connectDBView('SELECT * FROM employee');
+          break;
+          case "Add a department":
+            inquirer.prompt([
+              {
+                name: 'department',
+                message: "Department's name:",
+                type: 'input'
+              }
+            ]).then((answer) =>{
+              let command = `INSERT INTO department (name) VALUES ('${answer.department}')`;
+              connectDBEdit(command);
+            })
+            break;
+            case "Add a role":
+              inquirer.prompt([
+                {
+                  name: 'title',
+                  message: "Role's name:",
+                  type: 'input'
+                },
+                {
+                  name: 'salary',
+                  message: "Enter salary",
+                  type: 'input',
+                },
+                {
+                  name: 'departmentid',
+                  message: "Enter the department ID",
+                  type: 'input',
+                }
+              ]).then((answer) =>{
+                let command = `INSERT INTO role (name) VALUES ('${answer.role}')`;
+                connectDBEdit(command);
+              })
+              break;
+              case "Add an employee":
+                inquirer.prompt([
+                  {
+                    name: 'role',
+                    message: "Employee's name:",
+                    type: 'input'
+                  }
+                ]).then((answer) =>{
+                  let command = `INSERT INTO employee (name) VALUES ('${answer.employee}')`;
+                  connectDBEdit(command);
+                })
+                break;
+                case "Update an employee role":
+                  updateEmployee();
+                  break;
       default:
-        console.log('Another option chosen');
+        console.log('Press Ctrl C to exit');
   }
 }
 )
-  
+}
+
+prompt();
--- schema.sql
+++ schema.sql
@@ -38,4 +38,30 @@ VALUES
 ('Engineering'),
 ('Finance'),
 ('Legal'),
-('Banking')
+('Banking');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+('Sales Lead', 60000, 1),
+('Salesperson', 35000, 1),
+('Lead Engineer', 80000, 2),
+('Software Engineer', 85000, 2),
+('Account Manager', 70000, 3),
+('Accountant', 45000, 3),
+('Legal Team Lead', 110000, 4),
+('Lawyer', 95000, 4),
+('Manager', 65000, 5),
+('Teller', 35000, 5);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+('Mike', 'Brown', 1, null),
+('Joe', 'Malone', 2, 1),
+('Brian', 'Griffen', 3, null),
+('Peter', 'Barker', 4, 3),
+('Eric', 'Jones', 5, null),
+('Sarah', 'Happy', 6, 5),
+('Michelle', 'Brooks', 7, null),
+('Mike', 'Williams', 8, 7),
+('Tara', 'Reed', 9, null),
+('Leslie', 'Perry', 10, 9);
